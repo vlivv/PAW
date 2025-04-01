eabcf876039b9580ec2d5190c2d575fef6ba0388
vlivv <150799007+vlivv@users.noreply.github.com>
2025-04-01 20:33:43 +0200

    przestrzenie nazw i automatyczne ładowanie klas

--- zadanie_5b/config.php
+++ zadanie_5b/config.php
@@ -7,3 +7,4 @@ $conf->action_root = $conf->app_root.'/ctrl.php?action=';
 $conf->action_url = $conf->server_url.$conf->action_root;
 $conf->app_url = $conf->server_url.$conf->app_root;
 $conf->root_path = dirname(__FILE__);
+
