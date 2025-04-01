425344bb89013f9b19093d4d9b15d59ac355c269
vlivv <150799007+vlivv@users.noreply.github.com>
2025-04-01 20:15:36 +0200

    nowa struktura

---
+++ zadanie_5a/config.php
@@ -0,0 +1,9 @@
+<?php
+$conf->server_name = 'localhost:80';
+$conf->server_url = 'http://'.$conf->server_name;
+$conf->app_root = '/zadanie_5a';
+$conf->action_root = $conf->app_root.'/ctrl.php?action=';
+
+$conf->action_url = $conf->server_url.$conf->action_root;
+$conf->app_url = $conf->server_url.$conf->app_root;
+$conf->root_path = dirname(__FILE__);
